6b467af378064a7df970ed9f37e5682fd5fd3704
emilysaffron <emiy.saffron@bbc.co.uk>
2021-01-17 01:21:30 +0000

    iniital pws page

---
+++ src/Pages/PastWorkouts.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const PastWorkouts = () => {
+  return <StyledDiv>Past Workouts Will Appear Here</StyledDiv>;
+};
+
+export default PastWorkouts;
