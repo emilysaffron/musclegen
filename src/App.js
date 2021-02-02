0fbbf0ca59304b0ea4814a8de21d21014928638f
emilysaffron <emiy.saffron@bbc.co.uk>
2021-02-02 11:08:51 +0000

    move firebase init

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@ import { useState } from "react";
 import DropDownNav from "./Components/DropdownNav";
 import fetchRoutes from "./Helpers/FetchRoutes";
 import { CurrentPlanContext } from "./Helpers/CurrentPlanContext";
-import firebaseConfig from "./firebaseConfig";
-import Firebase from "firebase";
-import styled from "@emotion/styled";
-
 
 function App() {
-  if (!Firebase.apps.length) {
-    // Initialising this in a component that doesn't have state changes (and therefore doesn't re-render) will allow you to call this without needing the condition
-    Firebase.initializeApp(firebaseConfig);
-  }
-
   const [currentPlan, setCurrentPlan] = useState(null);
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import firebaseConfig from "./firebaseConfig";
+import Firebase from "firebase";
+Firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
