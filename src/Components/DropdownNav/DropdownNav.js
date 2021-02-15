08c42737ecdc6965b7b47d0918800cff889e269c
emilysaffron <emiy.saffron@bbc.co.uk>
2021-02-15 17:21:17 +0000

    responsive design menu

--- src/Components/DropdownNav/DropdownNav.js
+++ src/Components/DropdownNav/DropdownNav.js
@@ -2,14 +2,23 @@ import styled from "@emotion/styled";
 import Menu from "../Menu/Menu";
 import { useState } from "react";
 const StyledModal = styled.div`
+  position: fixed;
   display: flex;
-  align-items: baseline;
   flex-direction: column;
   text-align: left;
   z-index: 100;
+  @media (max-width: 380px) {
+    background-color: rgb(242, 242, 242, 0.9);
+    text-align: center;
+    height: 100%;
+    width: 100%;
+    justify-content: center;
+    font-size: xxx-large;
+  }
 `;
 
 const OpenMenu = styled.div`
+  position: fixed;
   font-size: 20px;
   cursor: pointer;
   align-self: baseline;
@@ -20,24 +29,22 @@ const OpenMenu = styled.div`
 const DropDownNav = () => {
   const [navState, toggleNavState] = useState(false);
 
-  return (
-    <div
-      style={{ position: "fixed" }}
-      onMouseEnter={() => toggleNavState(!navState)}
-      onMouseLeave={() => toggleNavState(!navState)}
-    >
-      <OpenMenu onMouseEnter={() => toggleNavState(!navState)}>
-        OPTIONS
-      </OpenMenu>
-      {navState ? (
-        <StyledModal>
-          <Menu
-            options={["Home", "New Workout", "Past Workouts", "Videos"]}
-            dropdown={true}
-          />
-        </StyledModal>
-      ) : null}
-    </div>
+  return navState ? (
+    <StyledModal>
+      <div
+        onClick={() => toggleNavState(!navState)}
+        style={{ cursor: "pointer" }}
+      >
+        X
+      </div>
+
+      <Menu
+        options={["Home", "New Workout", "Past Workouts", "Videos"]}
+        dropdown={true}
+      />
+    </StyledModal>
+  ) : (
+    <OpenMenu onClick={() => toggleNavState(!navState)}>OPTIONS</OpenMenu>
   );
 };
 
