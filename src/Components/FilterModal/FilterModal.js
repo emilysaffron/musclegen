4f82c79aa0b00a202802654b1b3122760b111abd
emilysaffron <emiy.saffron@bbc.co.uk>
2021-02-17 23:06:38 +0000

    filter modal bug

--- src/Components/FilterModal/FilterModal.js
+++ src/Components/FilterModal/FilterModal.js
@@ -24,6 +24,7 @@ const StyledModal = styled.div`
     margin-left: 50px;
   }
 `;
+
 const OpenButton = styled.button`
   background: #cea8bb;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
@@ -47,27 +48,7 @@ const OpenButton = styled.button`
     margin-left: 50px;
   }
 `;
-const DoneButton = styled.button`
-  background: #cea8bb;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  color: black;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: max-content;
-  border: 2px solid black;
-  position: -webkit-sticky;
-  position: sticky;
-  top: 1rem;
-  border-radius: 20px;
-  cursor: pointer;
-  @media (max-width: 400px) {
-    z-index: 100;
-  }
-`;
+
 const ClearFilter = styled.button`
   display: flex;
   flex-direction: column;
@@ -84,14 +65,14 @@ const FilterModal = ({ filterExercises, half }) => {
   const [filterState, toggleFilterState] = useState(false);
 
   return filterState ? (
-    <StyledModal key={Math.random()}>
+    <StyledModal
+      key={Math.random()}
+      onClick={() => toggleFilterState(!filterState)}
+    >
       {getExerciseTargets(filterExercises, half)}
       <ClearFilter key={Math.random()} onClick={() => filterExercises("")}>
         Clear Filter
       </ClearFilter>
-      <DoneButton onClick={() => toggleFilterState(!filterState)}>
-        Done
-      </DoneButton>
     </StyledModal>
   ) : (
     <OpenButton onClick={() => toggleFilterState(!filterState)}>
